21c5a353caa718126711c2b089882be79ce7ff09
Manikanta2 <manikanta1596@gmail.com>
2018-10-01 15:29:25 -0700

    Adding changes

--- musicwebsite/urls.py
+++ musicwebsite/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from music import views
+
 urlpatterns = [
-    url(r'^admin/$', admin.site.urls),
-    url(r'^music/$',include('music.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^music/$',include('music.urls')),
+    url(r'^music/(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^music/(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+
+
 ]
