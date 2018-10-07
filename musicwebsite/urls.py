89233437e65ac4830f4ad17e0366c87d41c28037
Manikanta2 <manikanta1596@gmail.com>
2018-10-07 02:16:09 -0700

    Adding changes

--- musicwebsite/urls.py
+++ musicwebsite/urls.py
@@ -5,9 +5,11 @@ from music import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^music/$',include('music.urls')),
-    url(r'^music/(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^music/(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^music/',include('music.urls')),
+    #url(r'^music/(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    #url(r'^music/(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
 
 ]
